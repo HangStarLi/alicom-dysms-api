9036ce76dcb7405dfa422ff8f254c89f83851bfc
starLi <46918262+HangStarLi@users.noreply.github.com>
2019-05-03 21:34:15 +0800

    Update SmsDemo.java

--- src/main/java/com/alicom/dysms/api/SmsDemo.java
+++ src/main/java/com/alicom/dysms/api/SmsDemo.java
@@ -37,8 +37,8 @@ public class SmsDemo {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIIKj2ZOJRyAYM";
-    static final String accessKeySecret = "zfGJa1BGsOhV4IURJusXCTTEAPPEMC";
+    static final String accessKeyId = "LTAIIKj2";
+    static final String accessKeySecret = "zfGJa1BGsOhV4I";
 
     public static SendSmsResponse sendSms() throws ClientException {
 
@@ -56,9 +56,9 @@ public class SmsDemo {
         //必填:待发送手机号
         request.setPhoneNumbers("15034106064");
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("航云");
+        request.setSignName("航");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_163851968");
+        request.setTemplateCode("SMS_163858");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         request.setTemplateParam("{\"code\":\"1314\"}");
 /*   request.setTemplateParam("{\"code\":\"123\"}");*/
